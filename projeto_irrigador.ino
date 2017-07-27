608a5ca3fb6f1ae534a7d5235b2b057ffb6cf47e
vitorcamp <vitorcamp89@gmail.com>
2017-07-27 11:37:46 -0300

    Add files via upload
    Primeira versão com a ideia principal do projeto

---
+++ projeto_irrigador.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(11, OUTPUT); // configura pino como saída
+
+}
+
+void loop() {
+  // read the input on analog pin 0:
+    int sensorValue = analogRead(A0);
+    if sensorValue <=500{   //sensor vai de 1023 (mais úmido) até 340 (mais seco)
+      digitalWrite(13, HIGH);
+      delay(200);
+    }
+
+}
