93d1d8d9b45c288b5f9e87d0ca68704cea5d9ef5
vitorcamp <vitorcamp89@gmail.com>
2017-07-27 11:54:38 -0300

    If para desligar motor
    Antes só tinha o if para ligar o motor, sem a condição para desligar.

--- projeto_irrigador.ino
+++ projeto_irrigador.ino
@@ -6,8 +6,13 @@ void setup() {
 void loop() {
   // read the input on analog pin 0:
     int sensorValue = analogRead(A0);
-    if sensorValue <=500{   //sensor vai de 1023 (mais úmido) até 340 (mais seco)
+    if (sensorValue > 500){   //sensor vai de 1023 (mais úmido) até 340 (mais seco)
       digitalWrite(13, HIGH);
+      delay(200);
+    }
+
+     if (sensorValue < 500){   //sensor vai de 1023 (mais úmido) até 340 (mais seco)
+      digitalWrite(13, LOW);
       delay(200);
     }
 
