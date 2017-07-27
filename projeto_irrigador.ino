ac2a43296b85c870e1e7a57ac25fd6ef6e853c00
vitorcamp <vitorcamp89@gmail.com>
2017-07-27 12:33:54 -0300

    Adicionei serial para  visualizar saída do sensor
    Parte do código que está comentada é para testes dos valores do sensor

--- projeto_irrigador.ino
+++ projeto_irrigador.ino
@@ -6,14 +6,20 @@ void setup() {
 void loop() {
   // read the input on analog pin 0:
     int sensorValue = analogRead(A0);
-    if (sensorValue > 500){   //sensor vai de 1023 (mais úmido) até 340 (mais seco)
+    
+    if (sensorValue > 500){   //sensor vai de 1023 (mais seco) até 380 (mais úmido)
       digitalWrite(13, HIGH);
       delay(200);
     }
 
-     if (sensorValue < 500){   //sensor vai de 1023 (mais úmido) até 340 (mais seco)
+     if (sensorValue < 500){   
       digitalWrite(13, LOW);
       delay(200);
     }
-
+      
+      /* Para visualizar no hiperterminal os testes de úmidade (leitura do sensor)
+      Serial.begin(9600);
+      Serial.println(sensorValue);
+      delay(1);
+      */
 }
